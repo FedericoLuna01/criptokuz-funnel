ec9f631039fd2906a8c9b80362639616f4c8f8d9
Federico Luna <federicolun4@gmail.com>
2025-01-05 23:14:13 -0300

    update data

--- components/calendly.tsx
+++ components/calendly.tsx
@@ -12,7 +12,7 @@ const Calendly = () => {
           />
         </div>
         <div className="container h-full rounded-lg" id="calendly">
-          <div className="calendly-inline-widget mt-6 lg:mt-0 h-[650px] sm:h-[700px] md:h-[750px] xl:h-[720px] max-w-[1000px] rounded-md mx-auto" data-url="https://calendly.com/criptokuz/criptokuz-mentorship-1-a-1?hide_event_type_details=1&hide_gdpr_banner=1&primary_color=00f4ff"
+          <div className="calendly-inline-widget mt-6 lg:mt-0 h-[650px] sm:h-[700px] md:h-[750px] xl:h-[720px] max-w-[1000px] rounded-md mx-auto" data-url="https://calendly.com/criptokuz/criptokuz-mentorship-1-a-1-clon?hide_gdpr_banner=1&primary_color=00fbff"
             style={{
               colorScheme: 'white'
             }}
