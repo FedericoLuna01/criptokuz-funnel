bcdf20791df0c208cce04f29f8f24d52cc6847a5
Federico Luna <federicolun4@gmail.com>
2025-10-17 02:25:08 -0300

    Update image assets: replace entregables-1.webp and entregables-3.webp with new versions

--- app/thankyou/page.tsx
+++ app/thankyou/page.tsx
@@ -1,79 +1,72 @@
-import { Metadata } from "next";
-
+import type { Metadata } from "next";
+import BackgroundLayout from "@/components/background-layout";
 import Faq from "@/components/faq";
 import Hero from "@/components/hero";
-import Testimonials from "@/components/testimonials";
-import { Button } from "@/components/ui/button";
-import BackgroundLayout from "@/components/background-layout";
-import Badge from "@/components/ui/badge";
 import PixelTrackerLead from "@/components/pixel-tracker-lead";
+import Testimonials from "@/components/testimonials";
+import Badge from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 
 export const metadata: Metadata = {
-  title: "Criptokuz | ¡Gracias por aplicar!",
-  description: "Muchas gracias por aplicar a la mentoría de CriptoKuz.",
-  openGraph: {
-    title: "Criptokuz | ¡Gracias por aplicar!",
-    description: "Muchas gracias por aplicar a la mentoría de CriptoKuz.",
-    type: "website",
-    url: "https://criptokuz.com/thankyou",
-    images: [
-      {
-        url: "./og-image-thankyou.jpg",
-        width: 800,
-        height: 450,
-        alt: "Criptokuz | ¡Gracias por aplicar!",
-      },
-    ]
-  },
+	title: "Criptokuz | ¡Gracias por aplicar!",
+	description: "Muchas gracias por aplicar a la mentoría de CriptoKuz.",
+	openGraph: {
+		title: "Criptokuz | ¡Gracias por aplicar!",
+		description: "Muchas gracias por aplicar a la mentoría de CriptoKuz.",
+		type: "website",
+		url: "https://criptokuz.com/thankyou",
+		images: [
+			{
+				url: "./og-image-thankyou.jpg",
+				width: 800,
+				height: 450,
+				alt: "Criptokuz | ¡Gracias por aplicar!",
+			},
+		],
+	},
 };
 
 const ThankYouPage = () => {
-  return (
-    <>
-      <PixelTrackerLead />
-      <main>
-        <BackgroundLayout
-          className="flex items-center justify-center pt-10"
-        >
-          <Badge
-            text="Pasos anteriores a la llamada"
-          />
-          <Hero
-            isMain={false}
-            title="Paso #1 (OBLIGATORIO) Mira el siguiente video para confirmar tu llamada"
-            highlightWords={["paso #1"]}
-            video="https://player.vimeo.com/video/1077456855?h=ccbfc9f446&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
-          />
-          <Button
-            size="xl"
-            asChild
-          >
-            <a
-              href="https://wa.me/5492984376411?text=Buenas,%20acabo%20de%20agendar%20una%20llamada%20con%20vos%20mi%20nombre%20es"
-              target="_blank"
-              className="z-50 mb-10"
-            >
-              Confirmar llamada
-            </a>
-          </Button>
-        </BackgroundLayout>
-        <Hero
-          isMain={false}
-          title="Paso #2 (OBLIGATORIO)
+	return (
+		<>
+			<PixelTrackerLead />
+			<main>
+				<BackgroundLayout className="flex items-center justify-center pt-10">
+					<Badge text="Pasos anteriores a la llamada" />
+					<Hero
+						isMain={false}
+						title="Paso #1 (OBLIGATORIO) Mira el siguiente video para confirmar tu llamada"
+						highlightWords={["paso #1"]}
+						video="https://player.vimeo.com/video/1077456855?h=ccbfc9f446&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+					/>
+					<Button size="xl" asChild>
+						<a
+							href="https://wa.me/5492984376411?text=Buenas,%20acabo%20de%20agendar%20una%20llamada%20con%20vos%20mi%20nombre%20es"
+							rel="noopener"
+							target="_blank"
+							className="z-50 mb-10"
+						>
+							Confirmar llamada
+						</a>
+					</Button>
+				</BackgroundLayout>
+				<Hero
+					isMain={false}
+					title="Paso #2 (OBLIGATORIO)
           Nuestro proceso para ser rentable en menos de 90 dias"
-          video="https://player.vimeo.com/video/1091952386?h=9bae24dcc2&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
-          highlightWords={["Paso #2", "rentable", "90 dias"]}
-        />
-        <Testimonials
-          isMain={false}
-          title="Paso #3 (OBLIGATORIO)
+					video="https://player.vimeo.com/video/1091952386?h=9bae24dcc2&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+					highlightWords={["Paso #2", "rentable", "90 dias"]}
+				/>
+				<Testimonials
+					isMain={false}
+					title="Paso #3 (OBLIGATORIO)
           Mira nuestros casos de éxito de CriptoKuz"
-          highlightWords={["paso #3"]}
-        />
-        <Faq />
-      </main>
-    </>
-  );
+					highlightWords={["paso #3"]}
+				/>
+				<Faq />
+			</main>
+		</>
+	);
 };
 
 export default ThankYouPage;
--- components/calendly.tsx
+++ components/calendly.tsx
@@ -1,56 +1,63 @@
-"use client"
+"use client";
 
 import Script from "next/script";
-import Heading from "./ui/heading";
 import { useEffect } from "react";
+import Heading from "./ui/heading";
 
 const Calendly = () => {
-  useEffect(() => {
-    const handleCalendlyEvent = (e: MessageEvent) => {
-      if (e.data.event && e.data.event === "calendly.event_scheduled") {
-        if (typeof window !== 'undefined') {
-          import('react-facebook-pixel').then((ReactPixel) => {
-            const pixelId = "1262635791863284";
-            ReactPixel.default.init(pixelId);
-            ReactPixel.default.fbq("track", "Schedule");
-          }).catch((error) => {
-            console.error('Error loading Facebook Pixel for Schedule tracking:', error);
-          });
-        }
-      }
-    };
+	useEffect(() => {
+		const handleCalendlyEvent = (e: MessageEvent) => {
+			if (e.data.event && e.data.event === "calendly.event_scheduled") {
+				if (typeof window !== "undefined") {
+					import("react-facebook-pixel")
+						.then((ReactPixel) => {
+							const pixelId = "1262635791863284";
+							ReactPixel.default.init(pixelId);
+							ReactPixel.default.fbq("track", "Schedule");
+						})
+						.catch((error) => {
+							console.error(
+								"Error loading Facebook Pixel for Schedule tracking:",
+								error,
+							);
+						});
+				}
+			}
+		};
 
-    window.addEventListener("message", handleCalendlyEvent);
+		window.addEventListener("message", handleCalendlyEvent);
 
-    return () => {
-      window.removeEventListener("message", handleCalendlyEvent);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener("message", handleCalendlyEvent);
+		};
+	}, []);
 
-  return (
-    <>
-      <section className="flex items-center mx-auto flex-col mt-10 px-2" >
-        <div>
-          <Heading
-            title={`Agendá tu llamada`}
-            highlightWords={["llamada", "2 de 2"]}
-          />
-        </div>
-        <div className="container h-full rounded-lg" id="calendly">
-          <div className="calendly-inline-widget mt-6 lg:mt-0 h-[650px] sm:h-[700px] md:h-[750px] xl:h-[740px] max-w-[1000px] rounded-md mx-auto" data-url="https://calendly.com/criptokuz/criptokuz-mentorship-1-a-1-mauro-clon-1?hide_event_type_details=1&hide_gdpr_banner=1&primary_color=00fbff"
-            style={{
-              colorScheme: 'white'
-            }}
-          >
-          </div>
-        </div>
-      </section>
-      <Script
-        src="https://assets.calendly.com/assets/external/widget.js"
-        async
-      ></Script>
-    </>
-  );
+	return (
+		<>
+			<section className="flex items-center mx-auto flex-col mt-10 px-2">
+				<div>
+					<Heading
+						title={`Agendá tu llamada`}
+						highlightWords={["llamada", "2 de 2"]}
+					/>
+				</div>
+				<div className="container h-full rounded-lg" id="calendly">
+					<div
+						className="calendly-inline-widget mt-6 lg:mt-0 h-[650px] sm:h-[700px] md:h-[750px] xl:h-[740px] max-w-[1000px] rounded-md mx-auto"
+						data-url="https://calendly.com/criptokuz/criptokuz-mentorship-1-a-1-mauro-clon-1?hide_event_type_details=1&hide_gdpr_banner=1&primary_color=2f979f&text_color=000000&background_color=ffffff"
+						// 00fbff
+						style={{
+							colorScheme: "white",
+						}}
+					></div>
+				</div>
+			</section>
+			<Script
+				src="https://assets.calendly.com/assets/external/widget.js"
+				async
+			></Script>
+		</>
+	);
 };
 
 export default Calendly;
